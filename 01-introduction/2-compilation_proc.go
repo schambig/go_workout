40788cc577b10fabb6720ffa5cdc812df43c369b
Salomon Chambi <salomonchambi@gmail.com>
2024-04-27 10:24:24 -0500

    feat: use  or compile  and then run

--- 01-introduction/2-compilation_proc.go
+++ 01-introduction/2-compilation_proc.go
@@ -18,3 +18,16 @@ and lets us do things like print text to the console.
 `func main()`
 defines the main function. main is the name of the function that acts as the entry point for a Go program.
  */
+
+/* 
+- If you're developing and testing your Go script, using `go run <script>` is more convenient.
+  It compiles and runs your script in one step, making it easier for quick iterations.
+
+- If you've finished developing and are deploying your script or need it to run efficiently,
+  compiling it first with `go build <script>` and then running the compiled binary with `./<script>` is recommended.
+  This pre-compiled binary tends to start faster and doesn't require the Go toolchain to execute,
+  making it more suitable for production environments.
+
+Each method has its place. For development and testing, use `go run`.
+For deployment, compile first and then run the binary.
+ */
