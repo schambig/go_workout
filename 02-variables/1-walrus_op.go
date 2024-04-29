89d184ceda565df8437c085346a255362dcdd0d4
Salomon Chambi <salomonchambi@gmail.com>
2024-04-29 09:00:00 -0500

    feat: use short variable declaration (walrus operator)

--- 02-variables/1-walrus_op.go
+++ 02-variables/1-walrus_op.go
@@ -13,19 +13,19 @@ func main() {
 }
 
 /* 
-Inside a function (like the main function) the := short assignment statement can be used
-in place of a var declaration. The := operator infers the type of the new variable
+Inside a function (like the main function) the `:=` short assignment statement can be used
+in place of a var declaration. The `:=` operator infers the type of the new variable
 based on the value.
-It's colloquially called the walrus operator because it looks like a walrus... sort of.
+It's colloquially called the walrus (morsa) operator because it looks like a walrus... sort of.
 
 These two lines of code are equivalent:
 	var empty string
 	empty := ""
 
-numCars := 10 // inferred as an integer
-temperature := 0.0 // temperature is inferred as a float because it has a decimal
-var isFunny = true // inferred as a boolean
+	numCars := 10 // inferred as an integer
+	temperature := 0.0 // temperature is inferred as a float because it has a decimal
+	var isFunny = true // inferred as a boolean
 
 Outside of a function (in the global/package scope), every statement begins with
-a keyword (var, func, and so on) and so the := construct is not available.
+a keyword (var, func, and so on) and so the `:=` construct is not available.
  */
