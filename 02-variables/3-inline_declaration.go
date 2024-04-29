eae23ff7d69020fbfcf3e2f00dfc9cb0c745f3ff
Salomon Chambi <salomonchambi@gmail.com>
2024-04-28 22:41:11 -0500

    feat: use same line declarations

---
+++ 02-variables/3-inline_declaration.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	// declare here
+	averageOpenRate, displayMessage := .23, "is the average open rate of your messages"
+
+	fmt.Println(averageOpenRate, displayMessage) // 0.23 is the average open rate of your messages
+}
