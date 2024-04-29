4e692e1796ac895e769b3bc8bf6994eb84d03879
Salomon Chambi <salomonchambi@gmail.com>
2024-04-29 09:00:00 -0500

    feat: use go conditionals

---
+++ 02-variables/8-conditionals.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+func main() {
+	messageLen := 10
+	maxMessageLen := 20
+	fmt.Println("Trying to send a message of length:", messageLen, "and a max length of:", maxMessageLen)
+
+	// don't touch above this line
+
+	if messageLen > maxMessageLen {
+		fmt.Println("Message sent")
+	} else {
+		fmt.Println("Message not sent")
+	}
+}
+
+/* 
+`if` statements in Go do not use parentheses around the condition:
+	if height > 4 {
+		fmt.Println("You are tall enough!")
+	}
+
+`else if` and `else` are supported as you might expect:
+	if height > 6 {
+		fmt.Println("You are super tall!")
+	} else if height > 4 {
+		fmt.Println("You are tall enough!")
+	} else {
+		fmt.Println("You are not tall enough!")
+	}
+
+Here are some of the comparison operators in Go:
+	== equal to
+	!= not equal to
+	< less than
+	> greater than
+	<= less than or equal to
+	>= greater than or equal to
+ */
