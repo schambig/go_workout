fba204e16b48aeddcbe676b62aeca9c48e33f7ec
Salomon Chambi <salomonchambi@gmail.com>
2024-04-29 09:00:00 -0500

    feat: initialize variables with no assignment

--- 02-variables/0-basic_types.go
+++ 02-variables/0-basic_types.go
@@ -16,27 +16,29 @@ func main() {
 /* 
 Go's basic variable types are:
 
-bool
+	bool
 
-string
+	string
 
-int  int8  int16  int32  int64
-uint uint8 uint16 uint32 uint64 uintptr
+	int  int8  int16  int32  int64
+	uint uint8 uint16 uint32 uint64 uintptr
 
-byte // alias for uint8
+	byte // alias for uint8
 
-rune // alias for int32
-     // represents a Unicode code point
+	rune // alias for int32
+		 // represents a Unicode code point
 
-float32 float64
+	float32 float64
 
-complex64 complex128
+	complex64 complex128
 
-We talked about strings and ints previously,
+We talked about `strings` and `ints` previously,
 and those two types should be fairly self-explanatory.
-A bool is a boolean variable, meaning it has a value of true or false.
-The floating point types (float32 and float64) are used for numbers that are not integers
+
+A `bool` is a boolean variable, meaning it has a value of true or false.
+
+The floating point types `float32` and `float64` are used for numbers that are not integers
 -- that is, they have digits to the right of the decimal place, such as 3.14159.
-The float32 type uses 32 bits of precision,
-while the float64 type uses 64 bits to be able to more precisely store more digits.
+	The `float32` type uses 32 bits of precision,
+	while the `float64` type uses 64 bits to be able to more precisely store more digits.
  */
