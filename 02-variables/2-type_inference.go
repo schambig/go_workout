ad28559998f3c771c3751f00443518209faa292e
Salomon Chambi <salomonchambi@gmail.com>
2024-04-28 22:35:33 -0500

    feat: infer the type from the value on the right side

---
+++ 02-variables/2-type_inference.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	penniesPerText := 2.00
+
+	// don't edit below this line
+	fmt.Printf("The type of penniesPerText is %T\n", penniesPerText) // The type of penniesPerText is float64
+}
+
+/* 
+To declare a variable without specifying an explicit type (either by using the := syntax or var = expression syntax),
+the variable's type is inferred from the value on the right hand side.
+ */
